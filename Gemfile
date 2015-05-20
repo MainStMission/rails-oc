42efb37302941523ac067bc81590508d4f140e51
tbrooke <tom.brooke@gmail.com>
2015-05-20 08:16:33 -0400

    updated eveything

--- Gemfile
+++ Gemfile
@@ -3,14 +3,14 @@ HOST_OS = RbConfig::CONFIG["host_os"]
 
 source "https://rubygems.org" 
 
-gem 'rack', '1.5.2'
-gem 'rails', '4.1.7'
+gem 'rack'
+gem 'rails', '4.2'
 gem 'activeresource'
 gem 'sprockets', '2.11.0'
 gem 'protected_attributes'
 gem 'nokogiri'
 gem 'pg'
-gem 'squeel'
+# gem 'squeel'
 gem 'rails_setup'
 gem 'devise'
 gem 'permitters', '~> 0.0.1'
@@ -23,33 +23,28 @@ gem 'american_date'
 gem 'cancan'
 gem 'paper_trail', '~> 3.0.6'
 gem 'prawn-print', :git => 'git://github.com/barsoom/prawn-print.git'
-gem 'capistrano'
-gem 'simple-capistrano-unicorn'
-gem 'rvm-capistrano', '>=1.3.0.rc4'
 gem 'backup-task'
 gem 'unicorn'
 gem 'active_model_serializers'
 gem 'turbolinks'
 gem 'restful_json', '~> 4.2.0'
 gem "prawn-labels"
-
-
 gem 'uglifier', '>= 1.3.0'
 gem 'abstract', '~> 1.0.0'
-
 gem "Ascii85", "~> 1.0.2"
 gem 'coffee-rails', '~> 4.0.0'
 gem 'react-rails', '~> 1.0.0.pre', github: 'reactjs/react-rails'
 gem 'sprockets-coffee-react'
-gem 'bourbon'
+# gem 'bourbon'
+gem 'bootstrap-sass' 
 gem 'sassy-buttons'
-gem 'neat'
+# gem 'neat'
 gem 'font-awesome-rails'
-# gem 'font-awesome-sass'
+gem 'font-awesome-sass'
 gem 'kaminari-bootstrap'
 gem 'jquery-rails'
 gem 'haml-rails'
-
+gem 'sass-rails'
 
 gem 'attr_encrypted'
 gem 'decent_exposure'
@@ -65,8 +60,6 @@ group :development, :test do
   gem 'shoulda'
   gem 'launchy'
   gem 'rspec-rails', '~> 3.1.0'
-  gem 'sass-rails', '~> 4.0.2'
-  gem 'bootstrap-sass', '~> 3.1.1'
   gem 'faker'
   gem 'pry-rails'
   gem 'pdf-inspector', :require => "pdf/inspector"
@@ -75,9 +68,6 @@ group :development, :test do
   gem 'libnotify',  :require => RUBY_PLATFORM.include?("linux")  && "libnotify"
   gem 'rb-inotify', :require => RUBY_PLATFORM.include?("linux")  && "rb-inotify"
   gem 'factory_girl_rails'
-  gem 'rb-fchange',   :platform => :mswin
-  gem 'win32console', :platform => :mswin
-  gem 'rb-notifu',    :platform => :mswin
   gem 'better_errors'
   gem 'binding_of_caller'
   gem 'meta_request'
@@ -88,7 +78,6 @@ group :development, :test do
   gem 'timecop'
   gem 'sqlite3'
   gem 'foreman'
-  gem 'spork', '~> 1.0rc'
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,76 +9,79 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130712203415) do
+ActiveRecord::Schema.define(version: 20130712203415) do
 
-  create_table "admin_notes", :force => true do |t|
-    t.string   "resource_id",     :null => false
-    t.string   "resource_type",   :null => false
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "admin_notes", force: true do |t|
+    t.string   "resource_id",     null: false
+    t.string   "resource_type",   null: false
     t.integer  "admin_user_id"
     t.string   "admin_user_type"
     t.text     "body"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
-  add_index "admin_notes", ["admin_user_type", "admin_user_id"], :name => "index_admin_notes_on_admin_user_type_and_admin_user_id"
-  add_index "admin_notes", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
+  add_index "admin_notes", ["admin_user_type", "admin_user_id"], name: "index_admin_notes_on_admin_user_type_and_admin_user_id", using: :btree
+  add_index "admin_notes", ["resource_type", "resource_id"], name: "index_admin_notes_on_resource_type_and_resource_id", using: :btree
 
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+  create_table "admin_users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
   end
 
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+  add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
+  add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "donations", :force => true do |t|
+  create_table "donations", force: true do |t|
     t.integer  "donor_id"
     t.decimal  "weight"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_index "donations", ["donor_id"], :name => "index_donations_on_donor_id"
+  add_index "donations", ["donor_id"], name: "index_donations_on_donor_id", using: :btree
 
-  create_table "donors", :force => true do |t|
+  create_table "donors", force: true do |t|
     t.string   "first_name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "last_name"
     t.integer  "tools_id"
   end
 
-  create_table "households", :force => true do |t|
+  create_table "households", force: true do |t|
     t.string   "household_name"
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "box_type"
     t.text     "income1"
-    t.decimal  "inc_amt1",                :precision => 8, :scale => 2
+    t.decimal  "inc_amt1",                precision: 8, scale: 2
     t.text     "income2"
-    t.decimal  "inc_amt2",                :precision => 8, :scale => 2
+    t.decimal  "inc_amt2",                precision: 8, scale: 2
     t.text     "income3"
-    t.decimal  "inc_amt3",                :precision => 8, :scale => 2
+    t.decimal  "inc_amt3",                precision: 8, scale: 2
     t.text     "expense1"
-    t.decimal  "exp_amt1",                :precision => 8, :scale => 2
+    t.decimal  "exp_amt1",                precision: 8, scale: 2
     t.text     "expense2"
-    t.decimal  "exp_amt2",                :precision => 8, :scale => 2
+    t.decimal  "exp_amt2",                precision: 8, scale: 2
     t.text     "expense3"
-    t.decimal  "exp_amt3",                :precision => 8, :scale => 2
+    t.decimal  "exp_amt3",                precision: 8, scale: 2
     t.text     "option1"
     t.text     "opt_val1"
     t.text     "option2"
@@ -110,23 +113,23 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.boolean  "bool5"
     t.string   "bool_val5"
     t.string   "income4"
-    t.decimal  "inc_amt4",                :precision => 8, :scale => 2
-    t.decimal  "exp_amt4",                :precision => 8, :scale => 2
+    t.decimal  "inc_amt4",                precision: 8, scale: 2
+    t.decimal  "exp_amt4",                precision: 8, scale: 2
     t.string   "expense4"
     t.integer  "s_numb"
     t.integer  "s_box"
   end
 
-  add_index "households", ["household_name"], :name => "index_households_on_household_name", :unique => true
+  add_index "households", ["household_name"], name: "index_households_on_household_name", unique: true, using: :btree
 
-  create_table "missions", :force => true do |t|
+  create_table "missions", force: true do |t|
     t.string   "title"
     t.text     "message"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "neighbors", :force => true do |t|
+  create_table "neighbors", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "street"
@@ -134,10 +137,10 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.string   "state"
     t.string   "zip"
     t.string   "phone"
-    t.decimal  "monthly_income",          :precision => 8, :scale => 2
-    t.decimal  "food_stamps",             :precision => 8, :scale => 2
-    t.decimal  "rent",                    :precision => 8, :scale => 2
-    t.decimal  "utilities",               :precision => 8, :scale => 2
+    t.decimal  "monthly_income",          precision: 8, scale: 2
+    t.decimal  "food_stamps",             precision: 8, scale: 2
+    t.decimal  "rent",                    precision: 8, scale: 2
+    t.decimal  "utilities",               precision: 8, scale: 2
     t.datetime "residency_date"
     t.boolean  "spouse"
     t.integer  "house_rank"
@@ -146,8 +149,8 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.string   "proof_of_residency_type"
     t.datetime "date_of_proof"
     t.boolean  "smokes"
-    t.datetime "created_at",                                            :null => false
-    t.datetime "updated_at",                                            :null => false
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
     t.string   "encrypted_ssn"
     t.string   "encrypted_ssn_iv"
     t.string   "encrypted_ssn_salt"
@@ -162,40 +165,40 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.date     "birth_date"
   end
 
-  add_index "neighbors", ["household_id"], :name => "index_neighbors_on_household_id"
-  add_index "neighbors", ["last_name"], :name => "index_neighbors_on_last_name"
+  add_index "neighbors", ["household_id"], name: "index_neighbors_on_household_id", using: :btree
+  add_index "neighbors", ["last_name"], name: "index_neighbors_on_last_name", using: :btree
 
-  create_table "rails_admin_histories", :force => true do |t|
+  create_table "rails_admin_histories", force: true do |t|
     t.text     "message"
     t.string   "username"
     t.integer  "item"
     t.string   "table"
-    t.integer  "month",      :limit => 2
-    t.integer  "year",       :limit => 8
-    t.datetime "created_at",              :null => false
-    t.datetime "updated_at",              :null => false
+    t.integer  "month",      limit: 2
+    t.integer  "year",       limit: 8
+    t.datetime "created_at",           null: false
+    t.datetime "updated_at",           null: false
   end
 
-  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
-  create_table "time_cards", :force => true do |t|
+  create_table "time_cards", force: true do |t|
     t.integer  "volunteer_id"
     t.datetime "start_time"
     t.datetime "end_time"
   end
 
-  create_table "users", :force => true do |t|
+  create_table "users", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "password_digest"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
+    t.integer  "sign_in_count",          default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
@@ -204,27 +207,27 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.string   "username"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
-  create_table "versions", :force => true do |t|
-    t.string   "item_type",  :null => false
-    t.integer  "item_id",    :null => false
-    t.string   "event",      :null => false
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
     t.string   "whodunnit"
     t.text     "object"
     t.datetime "created_at"
   end
 
-  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
-  create_table "visits", :force => true do |t|
+  create_table "visits", force: true do |t|
     t.integer  "neighbor_id"
-    t.date     "visited_on",     :null => false
+    t.date     "visited_on",     null: false
     t.text     "items_received"
     t.text     "notes"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
     t.integer  "household_id"
     t.string   "starch"
     t.string   "cereal"
@@ -233,10 +236,10 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.boolean  "optionb"
   end
 
-  add_index "visits", ["household_id"], :name => "index_visits_on_household_id"
-  add_index "visits", ["visited_on"], :name => "index_visits_on_visited_on"
+  add_index "visits", ["household_id"], name: "index_visits_on_household_id", using: :btree
+  add_index "visits", ["visited_on"], name: "index_visits_on_visited_on", using: :btree
 
-  create_table "volunteers", :force => true do |t|
+  create_table "volunteers", force: true do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "email"
@@ -246,8 +249,8 @@ ActiveRecord::Schema.define(:version => 20130712203415) do
     t.string   "state"
     t.string   "zip"
     t.string   "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string   "group"
     t.string   "cell_phone"
     t.datetime "birthday"
